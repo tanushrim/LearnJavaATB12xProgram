4e88f3cf686386cfc487898a1234e4bb4c23cb03
Tanushri Mishra <tanushreemishra013@gmail.com>
2025-05-17 21:19:42 +0530

    Program Push

--- src/Ex_01_Java_Basic/Lab_001_HelloWorld.java
+++ src/Ex_01_Java_Basic/Lab_001_HelloWorld.java
@@ -1,4 +1,7 @@
 package Ex_01_Java_Basic;
 
 public class Lab_001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
---
+++ src/Ex_01_Java_Basic/Lab_002_Table_Of_5_Using_Printf.java
@@ -0,0 +1,18 @@
+package Ex_01_Java_Basic;
+
+public class Lab_002_Table_Of_5_Using_Printf {
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.printf("5*1=%d\n",num);
+        System.out.printf("5*2=%d\n",num*2);
+        System.out.printf("5*3=%d\n",num*3);
+        System.out.printf("5*4=%d\n",num*4);
+        System.out.printf("5*5=%d\n",num*5);
+        System.out.printf("5*6=%d\n",num*6);
+        System.out.printf("5*7=%d\n",num*7);
+        System.out.printf("5*8=%d\n",num*8);
+        System.out.printf("5*9=%d\n",num*9);
+        System.out.printf("5*10=%d\n",num*10);
+    }
+    }

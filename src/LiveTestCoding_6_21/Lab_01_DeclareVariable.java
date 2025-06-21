ecb32eb4bfbeb09e0f10d78edf28c9d45712de05
Tanushri Mishra <tanushreemishra013@gmail.com>
2025-06-21 11:35:31 +0530

    Program Push

---
+++ src/LiveTestCoding_6_21/Lab_01_DeclareVariable.java
@@ -0,0 +1,14 @@
+package LiveTestCoding_6_21;
+
+public class Lab_01_DeclareVariable {
+    public static void main(String[] args) {
+        int Integer= 25;
+        double Double= 3.14;
+        boolean Boolean= true;
+        char Character= 'A';
+        System.out.println("Integer is:" + Integer);
+        System.out.println("Double is:" + Double);
+        System.out.println("Boolean is:" + Boolean);
+        System.out.println("Character is:" + Character);
+    }
+}

5207cecb050b2c649415288e6717bfbe38552c85
Tanushri Mishra <tanushreemishra013@gmail.com>
2025-06-21 11:49:28 +0530

    Program Push

---
+++ src/LiveTestCoding_6_21/Lab_02_DemonstrateOperators.java
@@ -0,0 +1,13 @@
+package LiveTestCoding_6_21;
+
+public class Lab_02_DemonstrateOperators {
+    public static void main(String[] args) {
+        int num1= 10;
+        int num2=5;
+        System.out.println("Addition: " + (num1+num2));
+        System.out.println("Subtraction: " + (num1-num2));
+        System.out.println("Multiplication :" + (num1*num2));
+        System.out.println("Division: " + (num1/num2));
+        System.out.println("Module:" + (num1%num2));
+    }
+}
